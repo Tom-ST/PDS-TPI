4ed2802e016e0bc0a555b369df8c212cd8c0dfc9
Tom-ST <61022932+Tom-ST@users.noreply.github.com>
2024-06-03 22:04:58 -0300

    Update BPM_detect.m

--- codigo/BPM_detect.m
+++ codigo/BPM_detect.m
@@ -28,6 +28,7 @@ endif
 
 
 [y, Fs] = audioread(archivo_audio, [1 n_muestras]);
+t = (0:n_muestras-1) / Fs;
 
 %Convierto audio stereo a mono
 if size(y ,2) == 2
@@ -35,7 +36,9 @@ if size(y ,2) == 2
 endif
 
 
-plot(y);
+figure();
+plot(t,y);
+xlabel("Tiempo (s)");
 
 %ventana para transformada de fourier de corto tiempo: 10ms (por que 10? se podria calcular este valor?)
 %analizo la transformada de fourier en ventanas de 10 ms para detectar
@@ -45,6 +48,36 @@ plot(y);
 % de baja frecuencia pero alta amplitud.
 % uso G(x) = x^(1/2), o arcsin(x). no uso log porque no se comporta bien cerca del cero
 
-ventana = 0.001;% en segundos (10 ms)
-G = @(x) arcsin(x);
+tam_ventana = 0.001;% en segundos (10 ms)
+tam_ventana_m = fix(tam_ventana * Fs); %tamaño de la ventana en muestras
+
+## Funcion de compresion
+G = @(x) sqrt(x);
+
+num_ventanas = fix(length(y) / tam_ventana_m);
+
+fragmentos = zeros(num_ventanas, tam_ventana_m);
+
+for i = 1:num_ventanas
+  fragmentos(i,:) = y((i-1)*tam_ventana_m + 1 : i*tam_ventana_m);
+endfor
+
+
+fft_resultados = zeros(num_ventanas, tam_ventana_m);
+
+for i = 1:num_ventanas
+  fft_resultados(i, :) = G(abs(fft(fragmentos(i,:))));
+endfor
+
+
+
+##Calculo del flujo de la energia FALTA HACER
+E = zeros(num_ventanas,1);
+
+
+
+
+##figure();
+##plot(t,E);
+##title("Flujo de la energia");
 
